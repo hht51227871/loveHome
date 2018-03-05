6f61b571b7f949321fdac2b60a4cb45c235c92b7
hht512278771 <hht5122@163.com>
2018-03-05 15:05:55 +0800

    第三次提交

--- routers/router.go
+++ routers/router.go
@@ -1,10 +1,18 @@
 package routers
 
 import (
-	"loveHome/controllers"
 	"github.com/astaxie/beego"
+	"loveHome/controllers"
 )
 
 func init() {
-    beego.Router("/", &controllers.MainController{})
+	beego.Router("/", &controllers.MainController{})
+
+	//请求地域信息
+	beego.Router("/api/v1.0/areas", &controllers.AreaController{}, "get:GetAreaInfo")
+	//session
+	beego.Router("/api/v1.0/session", &controllers.SessionController{}, "get:GetSession")
+
+	//house/index  房屋首页列表
+	beego.Router("/api/v1.0/houses/index", &controllers.HousesIndexController{}, "get:GetHousesIndex")
 }
